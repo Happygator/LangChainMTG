1fabd73e1976fac08444bfb0ec8dc5fbe8353033
Charles Perng <193326+perng@users.noreply.github.com>
2023-05-21 14:49:07 -0700

    use dict for csv line

--- src/utils/get_card_info.py
+++ src/utils/get_card_info.py
@@ -1,8 +1,9 @@
 import requests
 import csv
-import pandas
+# import pandas
+import get_all_card_names
 cardFile = open("cards.csv", 'w')
-writer = csv.writer(cardFile)
+writer = csv.writer(cardFile, quoting=csv.QUOTE_ALL)
 importantKeys = ['name', 'mana_cost', 'type_line', 'oracle_text']
 header = ['name', 'mana_cost', 'type_line', 'oracle_text', 'power', 'toughness', 
 'name2', 'mana_cost2', 'type_line2', 'oracle_text2', 'power2', 'toughness2']
@@ -22,30 +23,66 @@ def get_card_info(card_name):
     
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
+    return {}        
+
+def flat(card_info):
+    row = {}
+    if 'card_faces' in card_info:
+        for i,face in enumerate(card_info['card_faces']):
+            for key in importantKeys:
+                row[key+str(i)] = face[key]
+    else:
+        for key in importantKeys:
+            row[key+'0'] = card_info[key]
+    return row
+
+def dictionaries_to_csv(dictionaries, output_file):
+    # Extract all unique keys from the dictionaries
+    keys = set().union(*dictionaries)
+
+    # Open the output CSV file in write mode
+    with open(output_file, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=keys)
+
+        # Write the header row
+        writer.writeheader()
+
+        # Write each dictionary as a row in the CSV file
+        for dictionary in dictionaries:
+            writer.writerow(dictionary)
+
+
+
 
 # Example usage
 if __name__ == '__main__':
-    writer.writerow(header)
+    cards_names = get_all_card_names.get_all_card_names()['data'][:100]
+    cards = [get_card_info(c) for c in cards_names]
+    cards = [flat(c) for c in cards]
+    dictionaries_to_csv(cards, "data/sample.csv")
 
-    card_name = "Suspicious Stowaway // Seafaring Werewolf"
 
-    card_info = get_card_info(card_name)
-    row = []
-    if 'card_faces' in card_info:
-        for face in card_info['card_faces']:
-            for key in importantKeys:
-                row.append(face[key])
-            if 'power' in card_info:
-                row += [face['power'], face['toughness']]
-            else:
-                row += [None, None]
-    else:
-        for key in importantKeys:
-            row.append(card_info[key])
-        if 'power' in card_info:
-            row += [card_info['power'], card_info['toughness']]
-        else:
-            row += [None, None]
-        row += [None] * 6
-    writer.writerow(row)
+    # writer.writerow(header)
+
+    # card_name = "Suspicious Stowaway // Seafaring Werewolf"
+
+    # card_info = get_card_info(card_name)
+    # row = []
+    # if 'card_faces' in card_info:
+    #     for face in card_info['card_faces']:
+    #         for key in importantKeys:
+    #             row.append(face[key])
+    #         if 'power' in card_info:
+    #             row += [face['power'], face['toughness']]
+    #         else:
+    #             row += [None, None]
+    # else:
+    #     for key in importantKeys:
+    #         row.append(card_info[key])
+    #     if 'power' in card_info:
+    #         row += [card_info['power'], card_info['toughness']]
+    #     else:
+    #         row += [None, None]
+    #     row += [None] * 6
+    # writer.writerow(row)
         
