27494e11a7c21502fff20e716dcebc76865c25cc
Happygator <perng.arthur@gmail.com>
2023-05-21 17:35:32 -0700

    adasd

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template, request
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.chains import RetrievalQA
+import api_key
 import os
 
-app = Flask(__name__)
 
+app = Flask(__name__)
+os.environ["OPENAI_API_KEY"] = api_key.api_key
+qa_chain = RetrievalQA.from_chain_type(
+    llm=OpenAI(),
+    retriever=vectordb.as_retriever(search_kwargs={'k': 7}),
+    return_source_documents=True
+)
 @app.route('/')
 def home():
     print(os.getcwd())
@@ -12,8 +23,8 @@ def home():
 def chat():
     user_message = request.form['user_message']
     # Add your chatbot logic here to generate a response
-    bot_response = "This is the bot's response."
-
+    result = qa_chain({'query': user_message})
+    bot_response = reuslt['result']
     return {'bot_response': bot_response}
 
 if __name__ == '__main__':
---
+++ load_vectors.py
@@ -0,0 +1,17 @@
+import os
+from langchain.document_loaders import CSVLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+import api_key
+os.environ["OPENAI_API_KEY"] = api_key.api_key
+loader = CSVLoader(file_path='./src/utils/data/sample.csv')
+documents = loader.load()
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+documents = text_splitter.split_documents(documents)
+vectordb = Chroma.from_documents(
+  documents,
+  embedding=OpenAIEmbeddings(),
+  persist_directory='./data'
+)
+vectordb.persist()
