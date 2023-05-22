621482f9a9c66ac3d8b7c94831a506f808fe601e
Happygator <perng.arthur@gmail.com>
2023-05-21 17:52:56 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,19 @@ from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.chains import RetrievalQA
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
 import api_key
 import os
 
 
 app = Flask(__name__)
 os.environ["OPENAI_API_KEY"] = api_key.api_key
-qa_chain = RetrievalQA.from_chain_type(
-    llm=OpenAI(),
-    retriever=vectordb.as_retriever(search_kwargs={'k': 7}),
-    return_source_documents=True
-)
+vectordb = Chroma(persist_directory="./data", embedding_function=OpenAIEmbeddings())
+
+retriever = vectordb.as_retriever()
+
+qa_chain = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=retriever)
 @app.route('/')
 def home():
     print(os.getcwd())
@@ -24,7 +26,7 @@ def chat():
     user_message = request.form['user_message']
     # Add your chatbot logic here to generate a response
     result = qa_chain({'query': user_message})
-    bot_response = reuslt['result']
+    bot_response = result['result']
     return {'bot_response': bot_response}
 
 if __name__ == '__main__':
